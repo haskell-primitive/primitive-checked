9f9dab4b9f193b4c1f5a8ab31d6a9990980723e7
Andrew Martin <andrew.thaddeus@gmail.com>
2019-06-07 16:18:09 -0400

    make this work with primitive-0.7

--- src/Data/Primitive.hs
+++ src/Data/Primitive.hs
@@ -2,18 +2,14 @@ module Data.Primitive (
   module Data.Primitive.Types,
   module Data.Primitive.Array,
   module Data.Primitive.ByteArray,
-  module Data.Primitive.Addr,
   module Data.Primitive.PrimArray,
   module Data.Primitive.SmallArray,
-  module Data.Primitive.UnliftedArray,
   module Data.Primitive.MutVar
 ) where
 
-import Data.Primitive.Addr
 import Data.Primitive.Array
 import Data.Primitive.ByteArray
 import Data.Primitive.MutVar
 import Data.Primitive.PrimArray
 import Data.Primitive.SmallArray
 import Data.Primitive.Types
-import Data.Primitive.UnliftedArray
